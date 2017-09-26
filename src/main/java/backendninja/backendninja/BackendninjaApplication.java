8c553137541f7e9200fd5f97f929922ed0584841
ICordoba <nachocordoba05@gmail.com>
2017-09-26 12:21:39 -0300

    Bases del Proyecto Spring

---
+++ src/main/java/backendninja/backendninja/BackendninjaApplication.java
@@ -0,0 +1,12 @@
+package backendninja.backendninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendninjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendninjaApplication.class, args);
+	}
+}
